b931fcd58262264bfecea4aa5c06405913a1fab8
jsattler <mail@sattlerjoshua.de>
2020-03-21 14:18:19 +0100

    Add EntityNotFoundException for responses

---
+++ src/main/java/de/helpnoweatlater/backend/web/exception/EntityNotFoundException.java
@@ -0,0 +1,18 @@
+package de.helpnoweatlater.backend.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class EntityNotFoundException extends ResponseStatusException {
+    public EntityNotFoundException(HttpStatus status) {
+        super(status);
+    }
+
+    public EntityNotFoundException(HttpStatus status, String reason) {
+        super(status, reason);
+    }
+
+    public EntityNotFoundException(HttpStatus status, String reason, Throwable cause) {
+        super(status, reason, cause);
+    }
+}
