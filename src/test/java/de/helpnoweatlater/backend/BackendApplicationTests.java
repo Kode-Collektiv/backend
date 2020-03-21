67e8edcbeb98be0fd8fd6c764bc35c9ad5aae952
jsattler <mail@sattlerjoshua.de>
2020-03-21 12:43:02 +0100

    Setup spring project with minimal dependencies

---
+++ src/test/java/de/helpnoweatlater/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.helpnoweatlater.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
