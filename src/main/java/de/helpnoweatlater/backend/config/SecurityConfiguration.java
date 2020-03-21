06a069176c4e177d11ff214820655d9b11a40459
jsattler <mail@sattlerjoshua.de>
2020-03-21 14:19:19 +0100

    Add SecurityConfiguration

---
+++ src/main/java/de/helpnoweatlater/backend/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package de.helpnoweatlater.backend.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // TODO: CORS, API Endpoint
+
+}
